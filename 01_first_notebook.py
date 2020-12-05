09f49caca65f30c4d17102feb67bfae1fa574377
Polycarps <70601280+Polycarps@users.noreply.github.com>
2020-12-05 12:02:20 -0500

    Add files via upload

---
+++ 01_first_notebook.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  Introduction 
+# "This is my *first* Jupyter notebook"
+
+# In[3]:
+
+
+x = 5
+y = 6
+z = x + y
+print(z)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
